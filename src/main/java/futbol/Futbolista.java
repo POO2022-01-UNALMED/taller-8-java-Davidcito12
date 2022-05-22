5e69ae034f726b46e772051fea6a0137063f98fc
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-05-22 16:24:23 -0500

    Man que pasa con el tostring wtf

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -19,9 +19,10 @@ public abstract class Futbolista implements Comparable<Object>{
 	///////////////
 	
 	public String toString() {
-		return "El futbolista" + nombre +
+		return "El futbolista " + nombre + 
 				" tiene " + edad + 
-				", y juega de " + posicion;	}
+				", y juega de " + posicion;
+	}
 	
 	public boolean equals(Futbolista futbolista) {
 		return this == futbolista;
